ab98cf21e356048764276ca7e4ec2c0715fb5c7b
Finley Graham <166124470+fingra03@users.noreply.github.com>
2024-04-08 03:15:39 +0100

    fix: adjust to make compatible with fedora platform

--- make.sh
+++ make.sh
@@ -7,8 +7,8 @@
 # The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-gcc -fPIC -fno-stack-protector -c src/main.c
+gcc -fPIC -fno-stack-protector -march=x86-64 -c src/main.c
 
-sudo ld -x --shared -o /usr/lib/x86_64-linux-gnu/security/pam_group112.so main.o
+sudo ld -x --shared -o /usr/lib64/security/pam_generic_login.so main.o
 
 rm main.o
